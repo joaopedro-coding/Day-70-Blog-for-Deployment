33a4b5458ba648a613d09c838e2c95543c13cf5f
João Pedro <joaopedrinho2909@hotmail.com>
2025-12-23 13:22:34 -0300

    add env files

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import hashlib
+import os
+from dotenv import load_dotenv
 
 
-
+load_dotenv()
 app = Flask(__name__)
 login_manager = LoginManager()  
 login_manager.init_app(app)
@@ -23,7 +25,7 @@ def gravatar_url(email, size=100, rating='g', default='retro'):
 
 
 app.jinja_env.filters['gravatar'] = gravatar_url
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -45,7 +47,7 @@ def admin(func):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", 'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -229,4 +231,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
